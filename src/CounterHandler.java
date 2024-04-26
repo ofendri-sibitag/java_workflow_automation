bb649771cbd10c2327a327fad91b887a4482bbf0
Omar Fendri <Omar.Fendri@sibitag.com>
2024-04-26 10:34:00 +0200

    Adding Dockerfile, 2 default tracking classes, and one Main class

---
+++ src/CounterHandler.java
@@ -0,0 +1,64 @@
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.net.HttpURLConnection;
+import java.net.URL;
+
+public class CounterHandler {
+
+    private static final String JOB_DESCRIPTION = System.getenv("JOB_DESCRIPTION");
+    private static final String JOB = System.getenv("JOB");
+    private static final String INSTANCE = System.getenv("INSTANCE");
+    private static final String METRIC_NAME = "my_metric_count";
+
+    public static void sendEvent() {
+        updateFile();
+        try {
+            BufferedReader br = new BufferedReader(new FileReader("/app/exec_count.txt"));
+            String counter = br.readLine().trim();
+            br.close();
+
+            String fetchWorkTimeUp = "# HELP " + METRIC_NAME + " " + JOB_DESCRIPTION +
+                    "\n# TYPE " + METRIC_NAME + " counter" +
+                    "\n" + METRIC_NAME + " " + counter;
+
+            String URI = "http://host.docker.internal:9091/metrics/job/" + JOB + "/instance/" + INSTANCE;
+            String metrics = fetchWorkTimeUp + "\n";
+
+            System.out.println("Sending counter request...");
+            URL url = new URL(URI);
+            HttpURLConnection conn = (HttpURLConnection) url.openConnection();
+            conn.setRequestMethod("POST");
+            conn.setRequestProperty("Content-Type", "text/plain");
+            conn.setDoOutput(true);
+            conn.getOutputStream().write(metrics.getBytes());
+            conn.getInputStream();
+            System.out.println("Counter request sent!");
+        } catch (IOException e) {
+            System.out.println("Counter request failed : " + e.getMessage());
+        }
+        System.out.println("--------------");
+    }
+
+    public static void updateFile() {
+        try {
+            BufferedReader br = new BufferedReader(new FileReader("/app/exec_count.txt"));
+            String firstLine = br.readLine().trim();
+            br.close();
+
+            if (firstLine.matches("\\d+")) {
+                int counter = Integer.parseInt(firstLine);
+                counter++;
+                FileWriter fw = new FileWriter("/app/exec_count.txt");
+                fw.write((Integer.toString(counter)));
+                fw.close();
+                System.out.println("File updated!");
+            } else {
+                System.out.println("Failed to update file!");
+            }
+        } catch (IOException e) {
+            System.out.println("Can't find counter file : " + e.getMessage());
+        }
+    }
+}
---
+++ src/UpMetricHandler.java
@@ -0,0 +1,38 @@
+import java.io.IOException;
+import java.net.HttpURLConnection;
+import java.net.URL;
+
+public class UpMetricHandler {
+    private static final String JOB_DESCRIPTION = System.getenv("JOB_DESCRIPTION");
+    private static final String JOB = System.getenv("JOB");
+    private static final String INSTANCE = System.getenv("INSTANCE");
+
+    public static void sendUp() {
+        String fetchWorkTimeUp = "# HELP up " + JOB_DESCRIPTION +
+                "\n# TYPE up gauge" +
+                "\nup 1";
+
+        String URI = "http://host.docker.internal:9091/metrics/job/" + JOB + "/instance/" + INSTANCE;
+        String metrics = fetchWorkTimeUp + "\n";
+
+        System.out.println("Sending up request...");
+
+        try {
+            URL url = new URL(URI);
+            HttpURLConnection conn = (HttpURLConnection) url.openConnection();
+            conn.setRequestMethod("POST");
+            conn.setRequestProperty("Content-Type", "text/plain");
+            conn.setDoOutput(true);
+            conn.getOutputStream().write(metrics.getBytes());
+            conn.getInputStream();
+            System.out.println("Up request sent!");
+        } catch (IOException e) {
+            System.out.println("Up request failed: " + e.getMessage());
+        }
+        System.out.println("--------------");
+    }
+
+    public static void main (String[] args) {
+        sendUp();
+    }
+}
